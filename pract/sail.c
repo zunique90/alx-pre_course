3c4027bbabb80a2df0684aa933c0bfcb04273ae7
charlesezebuike <ezebuike.charles0@gmail.com>
2022-10-15 12:29:11 +0000

    sail updated

--- pract/sail.c
+++ pract/sail.c
@@ -4,12 +4,41 @@ void go_south_east(int *lat, int *lon)
 	*lat = *lat - 1;
 	*lon = *lon + 1;
 }
-int main()
+void go_south_west(int *lat, int *lon)
 {
+	*lat = *lat - 0;
+	*lon = *lon - 2;
+}
+void go_north_east(int *lat, int *lon)
+{
+	*lat = *lat + 2;
+	*lon = *lon + 2;
+}
+void go_north_west(int *lat, int *lon)
+{
+	*lat = *lat + 0;
+	*lon = *lon - 2;
+}
+
+int main()
+{	
 	int latitude = 32;
 	int longitude = -64;
-	
+
+	printf("Ahoy matey's!! let's go south-east!!\n");
 	go_south_east(&latitude, &longitude);
+	printf("Avast! Now at [%d, %d]\n", latitude, longitude);
+
+	printf("\nAhoy matey's!! let's go south-west!!\n");
+	go_south_west(&latitude, &longitude);
+	printf("Avast! Now at [%d, %d]\n", latitude, longitude);
+
+	printf("\nAhoy matey's!! let's go north-east!!\n");
+	go_north_east(&latitude, &longitude);
+	printf("Avast! Now at [%d, %d]\n", latitude, longitude);
+
+	printf("\nAhoy matey's!! let's go north-west!!\n");
+	go_north_west(&latitude, &longitude);
 	printf("Avast! Now at [%d, %d]\n", latitude, longitude);
 	return (0);
 }
