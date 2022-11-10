16c360dd5aa15fb8e31c82b3ac58c4bcb0dc139a
charles ezebuike <ezebuike.charles0@gmail.com>
2022-11-09 23:17:55 -0800

    EOF fix0

--- shell_pract/cq_read.c
+++ shell_pract/cq_read.c
@@ -1,11 +1,16 @@
-#include <stdio.h>
+#include "main.h"
 
 char *cq_read(void)
 {
 	char *line = NULL;
 	size_t len = 0;
 
-	getline(&line, &len, stdin);
+	while (getline(&line, &len, stdin) != -1)
+	{
+		return (line);
+	}
+
+	exit(EXIT_SUCCESS);
 
 	return (line);
 }
