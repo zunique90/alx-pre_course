08d1ed0c396374a880b9559d1553418fd336eda8
charles ezebuike <ezebuike.charles0@gmail.com>
2022-11-16 08:04:00 +0000

    going

--- shell_pract/cq_tokenize.c
+++ shell_pract/cq_tokenize.c
@@ -1,28 +1,33 @@
-#include <string.h>
-#include <stdlib.h>
-
+#include "main.h"
+/**
+ * cq_tokenize - creates an array of tokens from user input
+ * @line: pointer to user string
+ * Return: pointer to array created
+ */
 char **cq_tokenize(char *line)
 {
-	int i = 0;
-	int cap = 16;
-	char **tokens = malloc(cap * sizeof(char *));
+	int i = cap = 0
+	char **tokens;
+	char *delim = " \n";
+	char *token;
 
-	char *delim = " \t\n\r";
-	char *token = strtok(line, delim);
+	cap = _splitstring(line);
+	if (!cap)
+		return (NULL);
 
+	tokens = malloc((cap + 1) * sizeof(char *));
+	if (tokens == NULL)
+		exit(1);
+
+	token = strtok(line, delim);
 	while (token != NULL)
 	{
-		tokens[i] = token;
-		i++;
-
-		if (i >= cap)
-		{
-			cap = (int) (cap * 1.5);
-			tokens = realloc(tokens, cap * sizeof(char *));
-		}
+		tokens[i] = _strdup(token);
 		token = strtok(NULL, delim);
+		i++;
 	}
 	tokens[i] = NULL;
-
 	return (tokens);
 }
+/**
+ *
--- shell_pract/main.h
+++ shell_pract/main.h
@@ -7,10 +7,20 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <signal.h>
+#include <fcntl.h>
+
+extern char **environ
 
 char **cq_tokenize(char *line);
 char *cq_read(void);
 int cq_exec(char **argv);
 int _strlen(char *s);
+char *_getenv(const char *name);
+int _env(void);
+void _puts(char *str);
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
 
 #endif /* MAIN_H */
